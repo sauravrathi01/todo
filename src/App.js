e5210a6459876a9212cbe00c3d158a55c0c2d4d4
Saurav Rathi <sauravrathi1831@gmail.com>
2024-10-17 12:42:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [listTodo, setListTodo] = useState([]);
+
+  // Load saved todos from localStorage when component mounts
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (savedTodos) {
+      setListTodo(savedTodos);
+    }
+  }, []);
+
+  // Save todos to localStorage whenever listTodo changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(listTodo));
+  }, [listTodo]);
+
+  let addList = (inputText) => {
+    if (inputText !== '') setListTodo([...listTodo, inputText]);
+  };
+
+  const deleteListItem = (key) => {
+    let newListTodo = [...listTodo];
+    newListTodo.splice(key, 1);
+    setListTodo([...newListTodo]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="center-container">
+        <TodoInput addList={addList} />
+        <h1 className="app-heading">TODO</h1>
+        <hr />
+        {listTodo.map((listItem, i) => (
+          <TodoList key={i} index={i} item={listItem} deleteItem={deleteListItem} />
+        ))}
+      </div>
     </div>
   );
 }
